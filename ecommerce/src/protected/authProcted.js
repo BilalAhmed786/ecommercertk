ecaeadb101b906f489c381ac5662efa03f7d0922
BilalAhmed786 <fasst.sallar@gmail.com>
2026-09-07 17:01:58 +0500

    refactor issue

--- ecommerce/src/protected/authProcted.js
+++ ecommerce/src/protected/authProcted.js
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useUserDetailsMutation } from '../app/apiauth';
 import { useNavigate } from 'react-router-dom';
 
 const AuthProtected = ({ Component }) => {
-  const [fetchUserDetails, { data, error, isLoading }] = useUserDetailsMutation();
-
+  const [fetchUserDetails, { isLoading }] = useUserDetailsMutation();
   const navigate = useNavigate();
 
   useEffect(() => {
     const checkUser = async () => {
       try {
         const res = await fetchUserDetails();
+
         if (res?.data) {
-          navigate('/'); 
-        } 
+          navigate('/');
+        }
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     };
 
     checkUser();
-  }, []);
+  }, [fetchUserDetails, navigate]);
 
-  // Optional loading state
-  if (isLoading) return <div>Checking authentication...</div>;
+  if (isLoading) {
+    return <div>Checking authentication...</div>;
+  }
 
   return <Component />;
 };
